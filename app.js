dc42e68039e58ccdc3fd0b4fc0be155ab2096059
Shiyu Wang <shiyu3169@gmail.com>
2019-01-07 15:58:01 -0500

    database and router

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ const exphdb = require("express-handlebars");
 const bodyParser = require("body-parser");
 const path = require("path");
 
+// Database
+const db = require("./config/database");
+
+// Test DB
+db.authenticate()
+  .then(() => console.log("Database connected..."))
+  .catch(err => console.log(err));
+
 const app = express();
 
 app.get("/", (req, res) => res.send("INDEX"));
+
+// Gig routes
+app.use("/gigs", require("./routes/gigs"));
 
 const PORT = process.env.PORT || 5000;
 
